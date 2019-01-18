383ca928997bfc12a7d185580219848384f9532f
woongs <ccw1021@naver.com>
2019-01-18 15:40:51 +0900

    first commit on not using storyboard

---
+++ YoutubeClone/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  YoutubeClone
+//
+//  Created by woong on 18/01/2019.
+//  Copyright © 2019 woong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = UIColor.red
+    }
+
+
+}
+
